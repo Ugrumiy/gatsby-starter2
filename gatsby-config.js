84bc4caaab54cbf8092ca51f716bb208b9cc1061
Ilya Voronov <vi@kodix.ru>
2019-03-31 22:04:05 +0300

    md4

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/servicemd`,
+        path: `${__dirname}/src/service`,
         name: 'data',
       },
     },
