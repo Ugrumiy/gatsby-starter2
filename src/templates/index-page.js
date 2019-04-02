4cab7213db8a68917a7f0571b525be6aa0f8e2d4
Ilya Voronov <vi@kodix.ru>
2019-04-02 17:24:29 +0300

    sc

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
-
+import styled from 'styled-components';
 import Layout from '@components/Layout';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 export const IndexPageTemplate = ({
   mainpitch,
-}) => (<h1 className="title">{mainpitch.title}</h1>);
+}) => (<Title>{mainpitch.title}</Title>);
 
 IndexPageTemplate.propTypes = {
   mainpitch: PropTypes.object,
--- src/templates/model-page.js
+++ src/templates/model-page.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
+import styled from "styled-components"
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 const Type1 = ({title, color, type}) => <div style={{backgroundColor: `${color}`}}><h3>{type}</h3><h4>{title}</h4></div>;
@@ -23,19 +24,11 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
-          </div>
-        </div>
-      </div>
+    <section>
+      <h2>
+        {title}
+      </h2>
+      <PageContent content={content} />
     </section>
   )
 }
